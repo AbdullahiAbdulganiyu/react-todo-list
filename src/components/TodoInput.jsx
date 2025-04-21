0084637b39adb2db930ca9880a23b266862ada9f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-21 16:20:15 +0100

    adding functionality to track new added todo

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TodoInput() {
+  const [newTodo, setNewTodo] = useState("");
+
+  function handleNewTodo(e) {
+    setNewTodo(e.target.value);
+  }
+
   return (
     <header>
-      <input type="text" placeholder="Enter todo..." />
+      <input
+        type="text"
+        placeholder="Enter todo..."
+        value={newTodo}
+        onChange={handleNewTodo}
+      />
       <button>Add</button>
     </header>
   );
