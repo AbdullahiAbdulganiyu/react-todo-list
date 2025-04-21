9c78798562043b471b711269bc050abb50e7149d
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-21 16:21:35 +0100

    lifting the todos to the parent component of todoinput and todolist

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
+  let todos = [
+    "Go to the Gym",
+    "Eat some food and vegies",
+    "Pick up the Kids from School",
+  ];
   return (
     <>
       <TodoInput />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import TodoCard from "./TodoCard";
 
 export default function TodoList() {
-  let todos = [
-    "Go to the Gym",
-    "Eat some food and vegies",
-    "Pick up the Kids from School",
-  ];
   return (
     <ul className="main">
       {todos.map((todo, index) => {
