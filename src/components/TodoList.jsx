3d4b607c14c58e6234c04c9942fad8de51f3d2a7
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-21 15:03:44 +0100

    creating an array of todos and mapping over them

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function TodoList() {
-  return <div>TodoList</div>;
+  let todos = [
+    "Go to the Gym",
+    "Eat some food and vegies",
+    "Pick up the Kids from School",
+  ];
+  return (
+    <ul>
+      {todos.map((todo, index) => {
+        return <li key={index}>{todo}</li>;
+      })}
+    </ul>
+  );
 }
