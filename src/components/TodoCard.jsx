40027e42813962da5830e2a2e259b47ecf77b384
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-22 14:57:49 +0100

    wrapping the items inside a button

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -6,8 +6,12 @@ export default function TodoCard(props) {
     <li className="todoItem">
       {children}
       <div className="actionsContainer">
-        <i className="fa-solid fa-pen-to-square"></i>
-        <i className="fa-solid fa-trash"></i>
+        <button>
+          <i className="fa-solid fa-pen-to-square"></i>
+        </button>
+        <button>
+          <i className="fa-solid fa-trash"></i>
+        </button>
       </div>
     </li>
   );
